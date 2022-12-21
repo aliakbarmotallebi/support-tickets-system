9efd3855e75a0b17ab4be3ac329be6c87eeb2569
motallebi.aliakbar <motallebi.aliakbar@gmail.com>
2022-12-21 17:05:40 +0330

    fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,13 +13,6 @@ class DashboardController extends Controller
         $openTickets = Ticket::opened()->count();
         $closedTickets = Ticket::closed()->count();
 
-        return (new TelegramBot)
-            ->setToken('569740318:AAFJU5LkwbIRC6ABfCz-ri7gbA0ojwbqfe4')
-            ->setMethod('sendMessage')
-            ->setChatId('-1001490183979')
-            ->setText('New comment on ticket '  . "\n comment: \n by Username: ")
-            ->send();
-
         return view('dashboard', compact('totalTickets', 'openTickets', 'closedTickets'));
     }
 }
