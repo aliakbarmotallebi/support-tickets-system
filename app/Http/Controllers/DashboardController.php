a7d40a2b56e9ea73a64a8f062f96e51a82d44d47
motallebi.aliakbar <motallebi.aliakbar@gmail.com>
2022-12-21 17:00:48 +0330

    fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\TelegramBot;
 use Coderflex\LaravelTicket\Models\Ticket;
 
 class DashboardController extends Controller
@@ -11,6 +12,13 @@ class DashboardController extends Controller
         $totalTickets = Ticket::count();
         $openTickets = Ticket::opened()->count();
         $closedTickets = Ticket::closed()->count();
+
+        (new TelegramBot)
+            ->setToken('569740318:AAFJU5LkwbIRC6ABfCz-ri7gbA0ojwbqfe4')
+            ->setMethod('sendMessage')
+            ->setChatId('-1001490183979')
+            ->setText('New comment on ticket '  . "\n comment: \n by Username: ")
+            ->send();
 
         return view('dashboard', compact('totalTickets', 'openTickets', 'closedTickets'));
     }
